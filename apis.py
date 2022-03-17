9fad85e589ebe71a4a37fdc4c5410c72015a24f7
pragnyakiri <pradgnya.kiri@dolcera.com>
2022-03-17 11:12:17 +0530

    commit

--- apis.py
+++ apis.py
@@ -1,6 +1,7 @@
 #import docker
 #from flask import Flask, request, jsonify
 import sys, os
+import time
 
 #app= Flask(__name__)
 #client=docker.from_env()
@@ -10,7 +11,7 @@ import sys, os
 def docker_deploy(CN,RAN):
     # select scenario of CN and RAN and then deploy the scenario
     if CN == 'free5gc' and RAN == 'UERANSIM':
-        print("free5gc and UERANSIM")
+        print("free5gc CN and UERANSIM RAN")
         os.chdir('../')
         #check if directory already exists
         if os.path.isdir('5-fi-docker'):
@@ -34,7 +35,7 @@ def docker_deploy(CN,RAN):
         print(pwd)
         #return "success",200
     elif CN == 'free5gc' and RAN == 'OAI':
-        print("free5gc and OAI RAN")
+        print("free5gc CN and OAI RAN")
         os.chdir('../')
         #check if directory already exists
         if os.path.isdir('5-fi-docker-oai'):
@@ -49,11 +50,33 @@ def docker_deploy(CN,RAN):
         print("make base")        
         os.system('sudo docker-compose build')
         os.system('sudo docker-compose up -d --remove-orphans')
- 
+        os.chdir('../..')
+        pwd=os.getcwd()
+        print(pwd)
+    elif CN == 'OAI' and RAN == 'OAI':
+        print("OAI CN and OAI RAN")
+        os.chdir('../')
+        #check if directory already exists
+        if os.path.isdir('openairinterface5g'):
+            print('True')
+        else:
+            print('False')    
+            os.system('git clone https://gitlab.eurecom.fr/oai/openairinterface5g')
+        os.chdir('openairinterface5g')
+        os.system('git checkout develop')
+        os.system('git pull')
+        os.chdir('ci-scripts/yaml_files/5g_rfsimulator')
+        os.system('docker-compose up -d mysql oai-nrf oai-amf oai-smf oai-spgwu oai-ext-dn')
+        print("CN is UP")   
+        time.sleep(20)     
+        os.system('docker-compose ps -a')
+        time.sleep(20)     
+        os.system('docker-compose ps -a')
+        #os.system('docker-compose down') 
 
 
 #start flask app
 #if __name__=='__main__':
 #    app.run(host = '0.0.0.0',port=sys.argv[1])
 
-docker_deploy('free5gc','OAI')
+docker_deploy('OAI','OAI')
