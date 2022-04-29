1566d015d23c5b2bdbf69d6b52f8cb391699b3a2
pragnyakiri <pradgnya.kiri@dolcera.com>
2022-04-29 15:42:59 +0530

    com

--- apis.py
+++ apis.py
@@ -204,9 +204,9 @@ def get_logs(client,id):
 
 ###############################################################
 # Exception Handler
-@app.exception_handler(ValidationError)
-def validation_exception_handler(request, exc):
-    return PlainTextResponse(str(exc), status_code=400)
+#@app.exception_handler(ValidationError)
+#def validation_exception_handler(request, exc):
+#    return PlainTextResponse(str(exc), status_code=400)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
